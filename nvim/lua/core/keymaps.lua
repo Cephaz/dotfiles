74f63ae2cdcfd957fd72e29c5b37ebaf89785394
Cephaz <pierrezhoou@gmail.com>
2025-08-26 15:36:15 +0200

    feat: neovim

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,22 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- save file
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- save file without auto-formatting
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- quit file
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- window splits sous <leader>w
+vim.keymap.set("n", "<leader>ws", "<C-w>s", opts)  -- split horizontal
+vim.keymap.set("n", "<leader>wv", "<C-w>v", opts)  -- split vertical
+vim.keymap.set("n", "<leader>wh", "<C-w>h", opts)  -- aller à gauche
+vim.keymap.set("n", "<leader>wj", "<C-w>j", opts)  -- aller en bas
+vim.keymap.set("n", "<leader>wk", "<C-w>k", opts)  -- aller en haut
+vim.keymap.set("n", "<leader>wl", "<C-w>l", opts)  -- aller à droite
+vim.keymap.set("n", "<leader>wq", "<C-w>q", opts) -- fermer la fenetre
+
---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,28 @@
+
+-- Explorer
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- Ne pas lazy-loader pour que le mapping soit dispo tout le temps
+    lazy = false,
+    config = function()
+      require("nvim-tree").setup({
+        hijack_netrw       = true,
+        update_cwd         = true,
+        view = { width = 30, side = "left" },
+        actions = {
+          open_file = {
+            resize_window = false,
+            window_picker = {
+              enable = false,
+            },
+          },
+        },
+      })
+      -- Mapping <leader>e pour toggle l’explorer
+      vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Explorer Files" })
+    end,
+  },
+}
+
